a8dbd6c2bbb18ccce67764a8ebddb159b3c85254
Mohsen bagheri <mohsenbgi1380@gmail.com>
2024-08-13 16:54:29 +0330

    Added persian and english pages

---
+++ src/SampleApp/EnglishPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace SampleApp
+{
+    public partial class EnglishPage : ContentPage
+    {
+
+        public EnglishPage()
+        {
+            InitializeComponent();
+
+            DarkBtn.Clicked += (s, e) => App.Current.UserAppTheme = AppTheme.Dark;
+            LightBtn.Clicked += (s, e) => App.Current.UserAppTheme = AppTheme.Light;
+        }
+    }
+}
---
+++ src/SampleApp/PersianPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace SampleApp
+{
+    public partial class PersianPage : ContentPage
+    {
+        public PersianPage()
+        {
+            InitializeComponent();
+
+            DarkBtn.Clicked += (s, e) => App.Current.UserAppTheme = AppTheme.Dark;
+            LightBtn.Clicked += (s, e) => App.Current.UserAppTheme = AppTheme.Light;
+        }
+    }
+}
