e7e7c8c565684d46896ec9c040d592b1d9c2d188
Mohsen bagheri <mohsenbgi1380@gmail.com>
2024-08-14 15:23:12 +0330

    Added font vazir to sample app

--- src/SampleApp/MauiProgram.cs
+++ src/SampleApp/MauiProgram.cs
@@ -14,6 +14,8 @@ namespace SampleApp
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Vazir.ttf", "Vazir");
+
                 })
                 .UseMauiDatePicker();
 
